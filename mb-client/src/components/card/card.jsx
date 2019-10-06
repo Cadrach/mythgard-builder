2e22db99695d5fff7f4062ba3e3aa77504080567
Cadrach <cadrach@gmail.com>
2019-10-06 22:03:38 +0200

    CLIENT - animate card click

--- mb-client/src/components/card/card.jsx
+++ mb-client/src/components/card/card.jsx
@@ -3,11 +3,33 @@ import React from "react";
 // Stylesheet Imports
 import "./stylesheets/card.css";
 
-const Card = (props) => (
-  <div id="container">
-      <div id="card" style={{backgroundImage: 'url(images/cards/s/'+props.data.card_image+')'}}>
-      </div>
-  </div>
-);
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {animate: false};
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = (e) => {
+        this.setState({animate: true});
+        setTimeout(() => this.setState({animate: false}), 500);
+    }
+
+    render(){
+        const {animate} = this.state;
+
+        const card = <div id="card" style={{backgroundImage: 'url(images/cards/s/'+this.props.data.card_image+')'}}></div>;
+        const animatedCard = React.cloneElement(card, {className: 'animated  fadeOutRight'})
+
+        return (
+            <div id="container" onClick={this.handleClick}>
+                {card}
+                {animate ? animatedCard:null}
+            </div>
+        )
+    }
+}
 
 export default Card;

b5a420a43caedf25584c73db209885b580d383d3
Cadrach <cadrach@gmail.com>
2019-11-01 02:59:47 +0100

    CLIENT - Better scrollbar (react-custom-scrollbars)

--- mb-client/src/components/deck/deckContent.jsx
+++ mb-client/src/components/deck/deckContent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { List} from "antd";
 import DeckLine from "./deckLine";
 import PerfectScrollbar from 'react-perfect-scrollbar';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import './stylesheets/deckContent.scss';
 import {inject, observer} from "mobx-react";
@@ -12,9 +13,11 @@ export default class DeckContent extends React.Component {
 
     render(){
         const deck = this.props.deckStore.selectedDeck;
-        return (<div className="deck-content">
+        return (<Scrollbars style={{ height: 300 }}>
+            <div className="deck-content">
                 {deck.cards.map(card => <DeckLine key={card.id} item={card} count={card.count}/>)}
             </div>
+        </Scrollbars>
         )
 
     }

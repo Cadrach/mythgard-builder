af85b9a2b5a458aef7c4282f02ae4f3c138dfef3
Cadrach <cadrach@gmail.com>
2019-11-15 00:48:12 +0100

    CLIENT - Decks List: Nicer badges

--- mb-client/src/components/decksList/decksColumnRarity.jsx
+++ mb-client/src/components/decksList/decksColumnRarity.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-import {Typography, Tooltip} from "antd";
+import {Typography, Tooltip, Badge} from "antd";
+import constants from "../../constants";
 const {Text} = Typography;
+
+const badgeStyle = {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 14,
+    textShadow: '0px 0px 3px #000,  0px 0px 3px #000,  0px 0px 3px #000',
+}
 
 const DecksColumnRarity = (props) => {
     const valueDck = props.row["dck_nb_" + props.rarity + "s"];
@@ -12,13 +20,18 @@ const DecksColumnRarity = (props) => {
         return "";
     }
     else if(valueUsr !== undefined){
+        const percent = Math.round(valueUsr/valueDck*100);
         if(valueUsr<valueDck){
             //We are missing some cards, show tooltip
-            return <Tooltip title={<span>You are missing {valueDck-valueUsr} {props.rarity}(s)</span>}>{valueUsr}/{valueDck}</Tooltip>
+            return <Tooltip title={<span>You are missing {valueDck-valueUsr} {props.rarity}(s)</span>}>
+                <Badge count={valueUsr +'/'+ valueDck} style={{backgroundColor: percent<50 ? constants.colors.red:constants.colors.orange, ...badgeStyle}}></Badge>
+            </Tooltip>
         }
         else{
             //We have everything
-            return <Tooltip title={<span>You have all requested {props.rarity}(s)</span>}>{valueUsr}/{valueDck}</Tooltip>
+            return <Tooltip title={<span>You have all requested {props.rarity}(s)</span>}>
+                <Badge count={valueUsr +'/'+ valueDck} style={{backgroundColor: constants.colors.green,  ...badgeStyle}}></Badge>
+            </Tooltip>
         }
     }
     else{
